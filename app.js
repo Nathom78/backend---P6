7784200bd4ee35a5d18c7d384b53990980a08cad
Nathom78 <99360304+Nathom78@users.noreply.github.com>
2022-06-27 12:17:28 +0200

    Cross origine
    plus précis juste pour le localhost:4200

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ mongoose.connect(uri, {
 
 /* configuration CORS */
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     res.setHeader('Cross-Origin-Resource-Policy', 'same-site');
